b835160845381360a30634dabd56fe2005995611
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-20 09:34:27 +0200

    Tweaked the route to be custom

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'games/guess_the_number'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/guess", to: 'games/guess_the_number'
 end

801e7a2aa6c4313a9be33df6915323aa180b74b3
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-20 10:13:27 +0200

    Added the game logic in the controller and outputted in the view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,5 +8,21 @@ class GamesController < ApplicationController
   	if  chars[0] == "a"
   		@message = "Hey #{@name.capitalize} your name starts with the first letter of the alphabet!"
   	end
+
+  	@guess = params["guess"]
+
+  	number = 36
+
+  	@win = ""
+  	@high = ""
+  	@low = ""
+
+	  if @guess.to_i == number
+	  	@win = "Aloooo, you live dangerously, maybe that's why you got it right! YOU WON THE GAME"
+	  elsif @guess.to_i < number
+	  	@high = "Guess higher"
+	  elsif @guess.to_i > number
+	  	@low = "Guess lower"
+	  end
   end
 end
